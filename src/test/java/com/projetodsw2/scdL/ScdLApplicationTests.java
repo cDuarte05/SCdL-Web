cdfb72fd47f11e40043281c8068f11beff34bd2e
Henrique <hcduarte200@gmail.com>
2025-09-30 20:44:54 -0300

    esqueleto do projeto

---
+++ src/test/java/com/projetodsw2/scdL/ScdLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetodsw2.scdL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScdLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
