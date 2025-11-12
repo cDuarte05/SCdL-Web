ae655b8c11b5e8f8eae62f29ae9566209f502e7e
Henrique Chaves Duarte <henrique.duarte@writesys.com.br>
2025-11-12 15:08:03 -0300

    arq trocados de lugar

---
+++ back_SCdl/src/main/java/com/SCdl/web/login/dto/LoginRequestDTO.java
@@ -0,0 +1,3 @@
+package com.SCdl.web.login.dto;
+
+public record LoginRequestDTO (String email, String password){}
---
+++ back_SCdl/src/main/java/com/SCdl/web/login/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package com.SCdl.web.login.dto;
+
+public record RegisterRequestDTO(String name, String email, String password) {
+}
